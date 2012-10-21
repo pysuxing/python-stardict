31dbc382f8f16d65af3284cddacd7186d77b5317
SuXing <pysuxing@gmail.com>
2012-10-21 19:59:16 +0800

    Add main file "stardict.py" and distribute file "setup".

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="stardict",
+      description="Python Module reading StarDict dictionaries."
+      requires=["struct", "types", "gzip"],
+      provides=["stardict"],
+      version="0.1",
+      py_modules=["stardict"],
+      author="SuXing",
+      author_email="pysuxing@gmail.com")
